b65e70ea86d0a819c4e8c6b735ef3509713aaceb
Joe Gutierrez <josephpaulgutierrez@gmail.com>
2016-08-08 14:36:21 -0600

    Generated json files

--- lib/character.rb
+++ lib/character.rb
@@ -1,7 +1,8 @@
 class Character
+  attr_reader :name, :frame_data
   attr_accessor :health, :stun, :taunt, :jump, :jump_forward, :jump_backwards
   attr_accessor :dash_forward, :dash_backwards
-  attr_reader :frame_data
+  attr_accessor :move_list_url
 
   def initialize(name)
     @name = name
@@ -21,19 +22,32 @@ class Character
     ].include?(name.downcase)
   end
 
+  def metadata
+    @metadata ||= {
+      h: @health || '-',
+      s: @stun || '-',
+      t: @taunt || '-',
+      j: @jump || '-',
+      jf: @jump_forward,
+      jb: @jump_backwards,
+      df: @dash_forward,
+      db: @dash_backwards
+    }
+  end
+
+  def moves
+    attack_frames.map { |frame_data| frame_data.keys }.flatten
+  end
+
+  def attack_frames
+    frame_data.map(&:hashify)
+  end
+
   def to_json
     {
-      metadata_frames: {
-        h: @health || '-',
-        s: @stun || '-',
-        t: @taunt || '-',
-        j: @jump || '-',
-        jf: @jump_forward,
-        jb: @jump_backwards,
-        df: @dash_forward,
-        db: @dash_backwards
-      },
-      attack_frames: frame_data.map(&:hashify)
+      metadata_frames: metadata,
+      attack_frames: attack_frames,
+      move_list_url: move_list_url
     }.to_json
   end
 end
